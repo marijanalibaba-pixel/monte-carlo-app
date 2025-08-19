d88b39297ebf2ca20f26d785db48e4ef2d97a16b
marijanalibaba-pixel <marijan.alibaba@gmail.com>
2025-08-19 21:53:19 +0000

    PWA: add SW + manifest + icons + registration

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,2 +1,3 @@
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", (e) => self.clients.claim());
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+// Minimalno da PWA bude "installable"
