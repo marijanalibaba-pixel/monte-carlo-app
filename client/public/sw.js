3fd8e1911fa83af951995092e180824d163dd60b
marijanalibaba <46585719-marijanalibaba@users.noreply.replit.com>
2025-08-20 07:33:05 +0000

    Restored to '144f06ea35132f298de2f9f1fd4f1f3d72cee9f5'
    Replit-Restored-To: 144f06ea35132f298de2f9f1fd4f1f3d72cee9f5

--- client/public/sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => self.clients.claim());
-
-// Optional no-op fetch keeps things simple. Add real caching later if you want offline.
-self.addEventListener('fetch', () => {});
